98433ea358cf5d7892c6a63f16813da27fbe6e4d
R15Architect404 <R15Architect404@gmail.com>
2025-01-29 20:54:54 -0800

    Update SkyVR.lua

--- SkyVR.lua
+++ SkyVR.lua
@@ -268,12 +268,12 @@ do
 		local head = char:WaitForChild("Head")
 		local hum = char:FindFirstChildOfClass("Humanoid")
 		local continueTping = true
-		coroutine.wrap(function()
-			while continueTping do
+		--coroutine.wrap(function()
+			--while continueTping do
 				task.wait()
 				hrp.CFrame = headpart.CFrame
-			end
-		end)()
+			--end
+		--end)()
 
 
 		task.wait(0.25)	
